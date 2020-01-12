d43fee02a07bc71b4483a9ab28914a65c5604587
Alan Viana <alanlviana@hotmail.com>
2020-01-12 01:28:33 -0300

    Adicionados estudos de Interfaces e Delegates

---
+++ certificacao-csharp-pt1/Aula2 - criar tipos de referência/3 - Interfaces/Interfaces.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    public class Interfaces : IAulaItem
+    {
+        public void Executar()
+        {
+            Console.WriteLine("Aula de interfaces!");
+
+            IEletrodomestico eletro = new Televisao();
+            eletro.Ligar();
+            eletro.Desligar();
+
+            eletro = new Abajur();
+            eletro.Ligar();
+            eletro.Desligar();
+
+            eletro = new Radio();
+            eletro.Ligar();
+            eletro.Desligar();
+
+            eletro = new Lanterna();
+            eletro.Ligar();
+            eletro.Desligar();
+        }
+    }
+
+    interface IEletrodomestico
+    {
+        void Ligar();
+        void Desligar();
+    }
+
+    interface IIluminacao
+    {
+        double PotenciaDaLampada { get; set; }
+    }
+
+    class Televisao : IEletrodomestico
+    {
+        public void Desligar()
+        {
+            Console.WriteLine($"Desligando {this.GetType().Name}");
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine($"Ligando {this.GetType().Name}");
+        }
+    }
+
+    class Abajur : IEletrodomestico, IIluminacao
+    {
+        public double PotenciaDaLampada { get; set ; }
+
+        public void Desligar()
+        {
+            Console.WriteLine($"Desligando {this.GetType().Name}");
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine($"Ligando {this.GetType().Name}");
+        }
+    }
+
+    class Lanterna : IEletrodomestico, IIluminacao 
+    {
+        public double PotenciaDaLampada { get; set; }
+
+        public void Desligar()
+        {
+            Console.WriteLine($"Desligando {this.GetType().Name}");
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine($"Ligando {this.GetType().Name}");
+        }
+    }
+
+    class Radio : IEletrodomestico
+    {
+        public void Desligar()
+        {
+            Console.WriteLine($"Desligando {this.GetType().Name}");
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine($"Ligando {this.GetType().Name}");
+        }
+    }
+
+}
---
+++ certificacao-csharp-pt1/Aula2 - criar tipos de referência/4 - Delegates/Delegates.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    public class Delegates : IAulaItem
+    {
+        public void Executar()
+        {
+            Calculadora.Executar();
+        }
+    }
+
+    delegate double MetodoMultiplicacao(double input);
+
+    class Calculadora
+    {
+        static double Duplicar(double input)
+        {
+            return input * 2;
+        }
+        static double Triplicar(double input)
+        {
+            return input * 3;
+        }
+
+        public static void Executar()
+        {
+            Console.WriteLine($"Duplicar(7.5): {Duplicar(7.5)}");
+            Console.WriteLine($"Triplicar(7.5): {Triplicar(7.5)}");
+
+            Console.WriteLine("Método duplicar com delegate");
+            MetodoMultiplicacao metodo = Duplicar;
+            Console.WriteLine(metodo(7.5));
+            
+            Console.WriteLine("Método triplicar com delegate");
+            metodo = Triplicar;
+            Console.WriteLine(metodo(7.5));
+
+            Console.WriteLine("Método quadrado com delegate e método anonimo");
+            MetodoMultiplicacao metodoQuadrado = delegate (double input)
+            {
+                return input * input;
+            };
+            Console.WriteLine(metodoQuadrado(5));
+
+            Console.WriteLine("Método cubo com delegate e método anonimo (lambda)");
+            MetodoMultiplicacao metodoCuboLambda = (input) => {
+                return input * input * input;
+            };
+
+            Console.WriteLine(metodoCuboLambda(2));
+        }
+    }
+}
