8a7cb6e9728031b9bc07e6036cae23defb9b5f8f
Alan Viana <alanlviana@hotmail.com>
2020-01-13 08:38:22 -0300

    Adicionado estudo de dynamic e strings

---
+++ certificacao-csharp-pt1/Aula2 - criar tipos de referência/7 - Strings/Strings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    public class Strings : IAulaItem
+    {
+        public void Executar()
+        {
+            string a = "bom dia";
+            string b = "b";
+
+            b = b + "om dia";
+
+            Console.WriteLine($" a == b: {a == b}");
+            Console.WriteLine($"(object)a == (object)b: {(object)a == (object)b}");
+            
+            string bomDia = "bom dia";
+            char d = bomDia[4];
+            Console.WriteLine($"bomdia[4]: {bomDia[4]}");
+            Console.WriteLine($"d.GetType(): {d.GetType()}");
+        }
+    }
+
+}
+
