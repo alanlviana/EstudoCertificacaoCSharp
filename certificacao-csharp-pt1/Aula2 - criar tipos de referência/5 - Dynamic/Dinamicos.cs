69852199d38df03134f64e27cda5d6cf1a654864
Alan Viana <alanlviana@hotmail.com>
2020-01-12 23:35:12 -0300

    Implementados exemplos de uso de Object e Dynamic

---
+++ certificacao-csharp-pt1/Aula2 - criar tipos de referência/5 - Dynamic/Dinamicos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    public class Dinamicos : IAulaItem
+    {
+        public void Executar()
+        {
+            object objeto = 1;
+            //objeto = objeto + 3;
+            dynamic dinamico = 1;
+            dinamico = dinamico + 3;
+            Console.WriteLine($"dinamico = {dinamico}");
+
+            //dinamico.teste(); `int` não contém uma definição para `teste`
+        }
+    }
+
+}
+
---
+++ certificacao-csharp-pt1/Aula2 - criar tipos de referência/5 - Objetos/Objetos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    public class Objetos : IAulaItem
+    {
+        public void Executar()
+        {
+            int pontuacao = 10;
+            Console.WriteLine($"pontuacao: {pontuacao}");
+
+            Console.WriteLine("Objecto com valor primitivo");
+            object meuObjeto = pontuacao;
+
+            Console.WriteLine($"meuObjeto: {meuObjeto}");
+            Console.WriteLine($"meuObjeto.GetType(): {meuObjeto.GetType()}");
+
+            Console.WriteLine();
+            Console.WriteLine("Objecto com referência de objeto");
+            meuObjeto = new Jogador();
+            Jogador classRef;
+            classRef = (Jogador)meuObjeto; // conversão explicita ou cast
+            Console.WriteLine($"classRef.Pontuacao: {classRef.Pontuacao}");
+        }
+    }
+
+    class Jogador
+    {
+        public int Pontuacao { get; set; } = 10;
+    }
+}
+
