3e783d2ce02487bd14e01a4ea076aa8fc3942bd8
Alan Viana <alanlviana@hotmail.com>
2020-01-29 23:30:50 -0300

    Estudo com diferencas entre var, object, string e dynamic

---
+++ certificacao-csharp-pt1/Aula7 - string var object dynamic/1 - Usando Dynamic/UsandoDynamic.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class UsandoDynamic : IAulaItem
+    {
+        public void Executar()
+        {
+            // tipo definido na compilação
+            string s = "certificacao c#";
+            // tipo string definido na compilação, o tipo é definido por inferência, com baso no tipo atribuido
+            var v = "certificacao c#";
+            // tipo object definido na compilação, pode armazenar qualquer outro tipo
+            object o = "certificacão c#";
+
+            Console.WriteLine(s);
+            Console.WriteLine(v);
+            Console.WriteLine(o);
+
+            s = s.ToUpper();
+            v = v.ToUpper();
+            o = ((string)o).ToUpper();
+
+            Console.WriteLine(s);
+            Console.WriteLine(v);
+            Console.WriteLine(o);
+
+            //s = 123; // Cannot implicitly convert type 'int' to 'string'
+            //v = 123; // Cannot implicitly convert type 'int' to 'string'
+            o = 123;
+            o = (int)o + 4;
+
+            Console.WriteLine(o);
+
+            dynamic d = "certificacao c#";
+            Console.WriteLine(d);
+            d = d.ToUpper();
+            Console.WriteLine(d);
+
+            d = 123;
+            Console.WriteLine(d);
+
+            d = d + 4;
+            Console.WriteLine(d);
+
+
+        }
+    }
+
+}
